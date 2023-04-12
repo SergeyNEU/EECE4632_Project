1f4ac24afdba9bc31ac5af3c538d239b132b4606
Sergey Petrushkevich <73199264+SergeyNEU@users.noreply.github.com>
2023-04-12 13:52:24 -0400

    Update jupyter.py

--- Hardware/jupyter.py
+++ Hardware/jupyter.py
@@ -1,16 +1,24 @@
 from pynq import Overlay
 import numpy as np
-import random
 from pynq import allocate
 
 stream_overlay = Overlay('./aes128.bit')
 
 dma = stream_overlay.sergeyAES128.axi_dma_0
-input_buffer = allocate(shape=(11,), dtype=np.int32)
-output_buffer = allocate(shape=(11,), dtype=np.int32)
+input_buffer = allocate(shape=(2,), dtype=np.uint64)
+output_buffer = allocate(shape=(1,), dtype=np.uint64)
 
-input_buffer[:]=[random.randint(0,100) for k in range(11)]
-print(input_buffer)
+test_input = [
+    0x00112233445566778899AABBCCDDEEFF,
+    0xFFFFFFFFFFFFFFFF8899AABBCCDDEEFF
+]
+
+input_buffer[0] = test_input[0]
+input_buffer[1] = test_input[1]
+
+print("INPUT:")
+for value in input_buffer:
+    print(format(value, "016X"))
 
 def run_kernel():
     dma.sendchannel.transfer(input_buffer)
@@ -20,4 +28,6 @@ def run_kernel():
 
 run_kernel()
 
-print(output_buffer)
+print("OUTPUT:")
+for value in output_buffer:
+    print(format(value, "016X"))
