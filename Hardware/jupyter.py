cecddf2cf699f671eca8353d11729832b125f7c8
Sergey Petrushkevich <73199264+SergeyNEU@users.noreply.github.com>
2023-04-12 13:50:10 -0400

    Setting up...

--- Hardware/jupyter.py
+++ Hardware/jupyter.py
@@ -1,69 +1,23 @@
-from pynq import Overlay, MMIO
-import os
+from pynq import Overlay
+import numpy as np
 import random
-import struct
+from pynq import allocate
 
-# Load the bitstream and HWH files
-overlay = Overlay('/path/to/aes_overlay.bit')
+stream_overlay = Overlay('./aes128.bit')
 
-# Define MMIO addresses and size
-MMIO_BASE_ADDR = overlay.ip_dict['aes_encrypt_decrypt_0']['phys_addr']
-MMIO_SIZE = overlay.ip_dict['aes_encrypt_decrypt_0']['addr_range']
+dma = stream_overlay.sergeyAES128.axi_dma_0
+input_buffer = allocate(shape=(11,), dtype=np.int32)
+output_buffer = allocate(shape=(11,), dtype=np.int32)
 
-# Initialize MMIO instance
-mmio = MMIO(MMIO_BASE_ADDR, MMIO_SIZE)
+input_buffer[:]=[random.randint(0,100) for k in range(11)]
+print(input_buffer)
 
-# Function to generate a random 128-bit key
-def generate_key():
-    return [random.randint(0, 255) for _ in range(16)]
+def run_kernel():
+    dma.sendchannel.transfer(input_buffer)
+    dma.recvchannel.transfer(output_buffer)
+    dma.sendchannel.wait()
+    dma.recvchannel.wait()
 
-# Function to send data to FPGA
-def send_data(data, address):
-    for i in range(len(data)):
-        mmio.write(address + i, data[i])
+run_kernel()
 
-# Function to read data from FPGA
-def read_data(address, length):
-    return [mmio.read(address + i) for i in range(length)]
-
-# Function to encrypt and decrypt the input string
-def encrypt_decrypt(input_str):
-    # Pad input_str to a multiple of 16 bytes
-    padded_input = input_str.encode('utf-8')
-    padding_size = 16 - len(padded_input) % 16
-    padded_input += bytes([0] * padding_size)
-
-    # Generate a random 128-bit key
-    key = generate_key()
-
-    # Send data to FPGA
-    send_data(key, 0x10)
-    send_data(padded_input, 0x20)
-
-    # Start the encrypt and decrypt operation
-    mmio.write(0x00, 0x01)
-
-    # Wait for the operation to complete
-    while mmio.read(0x00) & 0x02 == 0:
-        pass
-
-    # Read the encrypted and decrypted data from FPGA
-    encrypted_data = read_data(0x30, 16)
-    decrypted_data = read_data(0x40, len(padded_input))
-
-    # Convert the data back to strings
-    encrypted_str = ''.join([chr(c) for c in encrypted_data])
-    decrypted_str = ''.join([chr(c) for c in decrypted_data if c != 0])
-
-    return encrypted_str, decrypted_str
-
-# Get user input
-input_str = input("Enter a string to encrypt and decrypt: ")
-
-# Encrypt and decrypt the input string
-encrypted_str, decrypted_str = encrypt_decrypt(input_str)
-
-# Print results
-print(f"Input string: {input_str}")
-print(f"Encrypted string: {encrypted_str}")
-print(f"Decrypted string: {decrypted_str}")
+print(output_buffer)
